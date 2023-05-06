ea49ecc4b2cfd8c2b2d48cd004284abdaafa33ab
Vladimir <vovachempogir@mail.ru>
2023-05-06 17:54:24 +0700

    1

---
+++ src/main/java/com/example/usermockito/UsermockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsermockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usermockito/UsermockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
